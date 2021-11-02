8bc87c28f99c50a08ed4a0197dcac1b1b0940fb5
Federico Baron <federicoagustinbaron@gmail.com>
2021-11-02 19:02:51 -0400

    add signup

---
+++ .history/composition-today/src/Signup_20211102183354.tsx
@@ -0,0 +1,41 @@
+import {useRef} from 'react'
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+export default function Signup(){
+
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+    const passwordConfirmRef = useRef<HTMLInputElement>(null);
+
+    return(
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className = "text-center mb-4">Sign up</h2>
+                    <Form>
+                        <Form.Group id = "email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type = "email" ref={emailRef} required/>
+                        </Form.Group>
+                        <Form.Group id = "password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type = "password" ref={passwordRef} required/>
+                        </Form.Group>
+                        <Form.Group id = "password-confirm">
+                            <Form.Label>Password Confirmation</Form.Label>
+                            <Form.Control type = "password" ref={passwordConfirmRef} required/>
+                        </Form.Group>
+                        <Button className = "w-100" type="submit">
+                            Sign Up
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                Already have an account? Log in
+            </div>
+        </>
+    )
+}
--- composition-today/src/App.tsx
+++ composition-today/src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import Signup from './Signup'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (<h1>Hello World</h1>);
+  return <Signup/>
 }
 
-export default App;
+export default App
