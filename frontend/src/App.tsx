cb0ca98ea4609971d0bda6182b27c17d1d42bf89
Federico Baron <federicoagustinbaron@gmail.com>
2021-11-10 13:08:58 -0500

    add skeleton pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,9 @@ import Home from './Home';
 import PrivateRoute from './PrivateRoute'
 import Login from './Login';
 import ForgotPassword from './ForgotPassword';
+import Blog from './Blog';
+import RelatedProjects from './RelatedProjects';
+import Showcase from './Showcase';
 
 function App() {
   return(    
@@ -20,6 +23,9 @@ function App() {
               <Route path="/signup" component={Signup}/>
               <Route path="/login" component={Login}/>
               <Route path="/forgot-password" component={ForgotPassword}/>
+              <Route path="/blog" component={Blog}/>
+              <Route path="/showcase" component={Showcase}/>
+              <Route path="/related-projects" component={RelatedProjects}/>
             </Switch>
           </AuthProvider>
         </Router>
