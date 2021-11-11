e71019b733a13a68a3c71e0d66d36298f1ec030f
Federico Baron <federicoagustinbaron@gmail.com>
2021-11-10 20:44:28 -0500

    add container box

--- frontend/src/MyProfile.tsx
+++ frontend/src/MyProfile.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import TopNavBar from './TopNavBar'
+import './MyProfileStyle.css'
 
 export default function MyProfile() {
     return (
         <>
             <TopNavBar/>
-            <Container className="d-flex align-items-center justify-content-center" style = {{minHeight:"100vh"}}>
+            <Container className="d-flex justify-content-center" style = {{minHeight:"100vh", padding:"5%"}}>
                 <div className="w-100" style={{maxWidth:"400px"}}>
-                    <div>
-                        My profile
+                    <div id="container">
+                        <div id="my-profile-box"></div>
+                        Username
                     </div>
+                    
                 </div>
             </Container>
         </>
