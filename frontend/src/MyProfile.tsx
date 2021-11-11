3a5f987d6d403ac05b1da768f60b2945b9f66461
Federico Baron <federicoagustinbaron@gmail.com>
2021-11-11 00:01:09 -0500

    add button dummies

--- frontend/src/MyProfile.tsx
+++ frontend/src/MyProfile.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Button, Container } from 'react-bootstrap'
 import TopNavBar from './TopNavBar'
 import './MyProfileStyle.css'
 
@@ -9,8 +9,12 @@ export default function MyProfile() {
             <TopNavBar/>
             <Container className="d-flex justify-content-center" style = {{minHeight:"100vh", padding:"3%"}}>
                 <div id="container">
-                    <div id="my-profile-box"></div>
                     <h1 style = {{padding: "2%", fontSize: "3vw"}}>Username</h1>
+                    <Button variant="light">Experience</Button>{' '}
+                    <Button variant="light">Music</Button>{' '}
+                    <Button variant="light">Events</Button>{' '}
+                    <Button variant="light">Articles</Button>{' '}
+                    <div id="my-profile-box"></div>
                 </div>
             </Container>
         </>
