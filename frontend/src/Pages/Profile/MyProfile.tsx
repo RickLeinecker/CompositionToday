c89deff43e3067f55d3970e68a9bb70b61ea5173
Federico Baron <federicoagustinbaron@gmail.com>
2021-11-11 10:07:19 -0500

    properly set current section

--- frontend/src/Pages/Profile/MyProfile.tsx
+++ frontend/src/Pages/Profile/MyProfile.tsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import { Button, Container } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, ButtonGroup, Container } from 'react-bootstrap'
 import TopNavBar from '../TopNavBar'
 import './MyProfileStyle.css'
 
 export default function MyProfile() {
+
+    const [currentSection, setCurrentSection] = useState<string>("Experience")
+    const handleClick=(event: any)=>{
+        event.preventDefault()
+        setCurrentSection(event?.target?.value)
+    }
+
     return (
         <>
             <TopNavBar/>
             <Container className="d-flex justify-content-center" style = {{minHeight:"100vh", padding:"3%"}}>
                 <div id="container">
-                    <h1 style = {{padding: "2%", fontSize: "3vw"}}>Username</h1>
-                    <Button variant="light">Experience</Button>{' '}
-                    <Button variant="light">Music</Button>{' '}
-                    <Button variant="light">Events</Button>{' '}
-                    <Button variant="light">Articles</Button>{' '}
-                    <div id="my-profile-box"></div>
+                    
+                    {/* <h1 style = {{padding: "2%", fontSize: "3vw"}}>Username</h1> */}
+                    <ButtonGroup onClick={handleClick}>
+                        <Button variant="light" value="Experience">Experience</Button>{' '}
+                        <Button variant="light" value="Music">Music</Button>{' '}
+                        <Button variant="light" value="Events">Events</Button>{' '}
+                        <Button variant="light" value="Articles">Articles</Button>{' '}
+                    </ButtonGroup>
+                    <div>
+                        {currentSection === "Experience" && <h1>Experience</h1>}
+                        {currentSection === "Music" && <h1>Music</h1>}
+                        {currentSection === "Events" && <h1>Events</h1>}
+                        {currentSection === "Articles" && <h1>Articles</h1>}
+                    </div>
+                    {/* <div id="my-profile-box"></div> */}
                 </div>
             </Container>
         </>
