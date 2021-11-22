9d1235ed634a237b2e46045f9a61066beeff9b13
Federico Baron <federicoagustinbaron@gmail.com>
2021-11-22 11:18:39 -0500

    add sass files

---
+++ mobile/lib/screens/authenticate/authenticate 2.dart
@@ -0,0 +1,23 @@
+import 'package:composition_today/screens/authenticate/register.dart';
+import 'package:composition_today/screens/authenticate/sign_in.dart';
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  bool showSignIn = true;
+  void toggleView() {
+    setState(() => showSignIn = !showSignIn);
+  }
+
+  Widget build(BuildContext context) {
+    if (showSignIn) {
+      return SignIn(toggleView: toggleView);
+    } else {
+      return Register(toggleView: toggleView);
+    }
+  }
+}
