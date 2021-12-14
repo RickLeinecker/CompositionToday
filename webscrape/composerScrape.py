1109f53fed26127a6239c37a78c6cfcda8e59346
Neo <neocamanga@knights.ucf.edu>
2021-12-14 15:46:01 -0500

    More progress

--- webscrape/composerScrape.py
+++ webscrape/composerScrape.py
@@ -1,12 +1,15 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
+from geopy import geocoders
 import requests
 import re
+import json
 
 # To use this scraper you need to:
 # pip install lxml
 # pip install beautifulsoup4
 # pip install requests
+# pip install geopy
 
 # There are three main pages that the scraper will use:
 # top_page      : http://www.compositiontoday.com/composers/default.asp
@@ -54,7 +57,7 @@ def find_alphabet(path):
         "lastName": full_name[0],
         "isPublisher": 0
     }
-    print(composer_dict)
+    # print(composer_dict)
 
     find_composer(composer_link["href"])
 
@@ -73,6 +76,8 @@ def get_user_profile(html):
     no_bio_text = "We currently have no biographical information on this artist."
     # print(no_bio_text in composer_profile.text)
 
+    # Unfortunately, no designated place on site for
+    # composer's website, so leaving it empty
     user_profile_dict = {
         "bio": "",
         "website": ""
@@ -81,18 +86,59 @@ def get_user_profile(html):
     if no_bio_text not in composer_profile.text:
         user_profile_dict.update({"bio": composer_profile.text})
     
-    print(user_profile_dict)
+    # print(user_profile_dict)
     return
 
 def get_content(html):
     composer_content = html.find('td', {'width': "100%"})
     # print(composer_content)
     # print(composer_content.find('a', string = re.compile("Showcase")))
-    past_performances = composer_content.find_all('div', {'style' : "width:250px;height:80"})
+
+    content_list = []
+
+    get_past_performances(composer_content)
+    # get_music(composer_content)
+    # get_sheets()
+
+    # Ignore external music sheets for now
+    content_dict = {
+
+    }
+
+def get_past_performances(html):
+    past_performances = html.find_all('div', {'style' : "width:250px;height:80"})
     # print(past_performances)
+
+    
+    for performance in past_performances:
+        get_calendar(performance.find('a'))
+
+def get_calendar(html):
+    # print(html)
+
+
     # Do a split on date
     # if 'pm' in 
     datetime_object = datetime.strptime('04 October 2014 at 9:30AM', '%d %B %Y at %I:%M%p')
-    print(datetime_object)
+    # print(datetime_object)
 
+
+# For audio, get it from side site
+# For list of works, get it from main page
+def get_music(html):
+    # print(html.find_all('div', class_ = "boxes")[0].find_next_sibling("div"))
+    # print(html.find('b', string = "Audio").parent.parent.text)
+    audio = html.find('b', string = "Audio").parent.next_sibling
+    audioText = ""
+
+    if 'No audio samples by this composer currently available.' not in audio:
+        audio = html.find('b', string = "Audio").parent.parent.get_text()
+        print()
+
+    print(type(audio))
+    html = [dom.text for dom in html.find_all('div', class_ = "boxes")]
+    print(html)
+    # audio = html.find()
+
+g = geopy
 start_scraping()
