2f8197371d532e404607ee5e845ded475b83eaab
Neo <neocamanga@knights.ucf.edu>
2021-12-14 16:49:57 -0500

    Added more documentation to PrivateRoute

--- frontend/src/FirebaseAuth/PrivateRoute.js
+++ frontend/src/FirebaseAuth/PrivateRoute.js
@@ -2,14 +2,23 @@ import React from 'react'
 import { useAuth } from "./AuthContext"
 import { Route, Redirect } from 'react-router-dom'
 
-export default function PrivateRoute({ component: Component, props: myProps = "", ...rest }) {
+/**
+ * PrivateRoute ensures user is logged in. When in use
+ * must include component prop. If using props in that
+ * component, ensure you are passing an object through
+ * it. Ex. props = {{property= "value", ...}}
+ * @param component JSX page component to render after routing there
+ * @param props [Optional] props to pass to the component
+ * @returns Rendering of the JSX.Element
+ */
+export default function PrivateRoute({ component: Component, props: myProps = {}, ...rest }) {
 
     const { currentUser } = useAuth()
     return (
         <Route {...rest} render={props => {
             console.log(myProps)
             return currentUser ? (
-                <Component myProps={myProps} {...props} />
+                <Component {...{...myProps, ...props}} />
             ) : (
                 <Redirect to="/login"></Redirect>
             )
