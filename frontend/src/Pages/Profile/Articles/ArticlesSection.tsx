1be25f72171df89844648d8d124c4ed5abda298e
Federico Baron <federicoagustinbaron@gmail.com>
2021-12-14 14:05:40 -0500

    add other sections

--- frontend/src/Pages/Profile/Articles/ArticlesSection.tsx
+++ frontend/src/Pages/Profile/Articles/ArticlesSection.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import GenericHandler from '../../../Handlers/GenericHandler';
+import { Content, GenericHandlerObject } from '../../../ObjectInterface';
 
 export default function ArticlesSection() {
+
+    const [response, setResponse] = useState<Array<Content> | undefined>(undefined);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+
+    useEffect(() => {
+        async function fetchData(){
+
+            const handlerObject: GenericHandlerObject = {
+                data: JSON.stringify({contentType: "music"}),
+                methodType: "POST",
+                path: "getContentByType",
+            }
+
+            try{
+                let answer = (await GenericHandler(handlerObject));
+                if(answer.error.length > 0){
+                    console.log("error");
+                    return;
+                }
+                
+                setError(false);
+                setResponse(await answer.result);
+                setLoading(false);
+                
+
+            } catch(e: any){
+                console.error("Frontend Error: " + e);
+                setError(true);
+            }
+        
+        }
+        fetchData();
+    }, [])
+
+
+        
     return (
-        <div>
-            Articles
-        </div>
+        <>
+            <div>
+                {!error && loading ? <div>...loading</div> 
+                :
+                error ? 
+                <div>Could not process this request, please reload the page</div> 
+                : 
+                <div>
+                    {response?.map((_result: Content) => (
+                        <li key={_result.id}>
+                            <div>
+                                <p>{_result.contentText}</p>
+                            </div>
+                        </li>
+                    ))}
+                </div>
+                }
+            </div>
+        </>
     )
 }
