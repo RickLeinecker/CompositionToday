5542b03a5c26ee24988c666446636af6805c016d
Federico Baron <federicoagustinbaron@gmail.com>
2021-12-15 14:32:22 -0500

    clean up code

--- frontend/src/Pages/Profile/MyProfile.tsx
+++ frontend/src/Pages/Profile/MyProfile.tsx
@@ -1,25 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import { Alert, Button, ButtonGroup, Container, Image } from 'react-bootstrap'
+import { Alert, Container } from 'react-bootstrap'
 import TopNavBar from '../TopNavBar'
-import ArticlesSection from './Articles/ArticlesSection'
-import EventsSection from './Events/EventsSection'
-import ExperienceSection from './Experience/ExperienceSection'
-import MusicSection from './Music/MusicSection'
 import './MyProfileStyle.scss'
 import DefaultValues from '../../Styles/DefaultValues.module.scss'
 import { getAuth } from 'firebase/auth'
 import { GenericHandlerObject, User } from '../../ObjectInterface'
 import GenericHandler from '../../Handlers/GenericHandler'
+import MyProfileContentSelector from './MyProfileContentSelector'
 
 export default function MyProfile(props: any) {
 
-    const [currentSection, setCurrentSection] = useState<string>("Experience")
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(true);
     const [user, setUser] = useState<User>();
 
+    // get user info
     useEffect(() => {
-        // get user info
         async function fetchUser(){
             const handlerObject: GenericHandlerObject = {
                 data: JSON.stringify({uid: getAuth().currentUser?.uid}),
@@ -49,42 +45,7 @@ export default function MyProfile(props: any) {
         
         }
         fetchUser();
-
-        // sets current section button color to selected 
-        let property = document.getElementById(currentSection)
-
-        if(property != null)
-            property.style.background = DefaultValues.secondaryColor
-        console.log(property + " this is property")
-
-        return () => {
-        }
-    }, [currentSection])
-
-    const handleClick = async (event: any)=>{
-
-        event.preventDefault()
-        
-        // sets old section button color to selected
-        // and updates section 
-        if(event?.target?.value != null && event?.target?.value !== currentSection){
-            let oldProperty = document.getElementById(currentSection)
-            if(oldProperty != null){
-                oldProperty.style.background = DefaultValues.white
-            }
-            setCurrentSection(event?.target?.value)
-        }
-    }
-
-    function getUser(){
-        var user = getAuth().currentUser;
-        var email = user?.email
-
-        return(
-            <h1 id="userDisplay" style = {{padding: "2%", fontSize: "3vw"}}>{email}</h1>
-        )
-    }
-      
+    },[])
 
     return (
         <>
@@ -95,27 +56,7 @@ export default function MyProfile(props: any) {
                     <div>...loading</div> 
                 :
                 (user && !error) ? 
-                    <>
-                        <div id="container">
-                            <div style={{ display: "flex", marginLeft: "5%" }}>
-                                <Image style={{ width: "10%", height: "auto" }} src="img_avatar.png" roundedCircle />
-                                {getUser()}
-                            </div>
-                            <ButtonGroup className="buttonContainer" onClick={handleClick}>
-                                <Button className="rounded-pill" id="Experience" style={{background: DefaultValues.secondaryColor}} variant="light" value="Experience">Experience</Button>{' '}
-                                <Button className="rounded-pill" id="Music" variant="light" value="Music">Music</Button>{' '}
-                                <Button className="rounded-pill" id="Events" variant="light" value="Events">Events</Button>{' '}
-                                <Button className="rounded-pill" id="Articles" variant="light" value="Articles">Articles</Button>{' '}
-                            </ButtonGroup>
-                            <div id="my-profile-box"></div>
-                        </div>
-                        <div id="sections">
-                                {currentSection === "Experience" && <ExperienceSection userID={user.id} />}
-                                {currentSection === "Music" && <MusicSection />}
-                                {currentSection === "Events" && <EventsSection />}
-                                {currentSection === "Articles" && <ArticlesSection />}
-                        </div>
-                    </>
+                    <MyProfileContentSelector user={user}/>
                 :
                     <Alert variant="danger">{error}</Alert>
                 }
