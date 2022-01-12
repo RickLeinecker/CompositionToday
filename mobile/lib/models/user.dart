1d694b91ce2f1608052a948a6a3db9182d6400de
MDiDeo <mikeydideo@gmail.com>
2022-01-12 14:39:28 -0500

    updating user to match webapp
    changing the format of User to match the user in the webapp

--- mobile/lib/models/user.dart
+++ mobile/lib/models/user.dart
@@ -5,11 +5,22 @@ class MyUser {
 }
 
 class UserData {
+  final int? id;
   final String? uid;
   final String? firstName;
   final String? lastName;
   final String? username;
+  final String? email;
   final String? role;
+  final String? userProfileID;
 
-  UserData({this.uid, this.firstName, this.lastName, this.username, this.role});
+  UserData(
+      {this.id,
+      this.uid,
+      this.firstName,
+      this.lastName,
+      this.username,
+      this.email,
+      this.role,
+      this.userProfileID});
 }
