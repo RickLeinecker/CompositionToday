874fc3e6f8e1324ac17b45654515eb9c1d49b1de
Neo <neocamanga@knights.ucf.edu>
2022-01-12 01:20:22 -0500

    Adding more style to the papers and sliders

--- frontend/src/Pages/Showcase/Showcase.tsx
+++ frontend/src/Pages/Showcase/Showcase.tsx
@@ -1,23 +1,10 @@
-import { Box, Grid, Paper, Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import { Container } from 'react-bootstrap'
 import GenericSearch from '../../Helper/Generics/GenericSearch'
 import TopNavBar from '../TopNavBar'
-import DefaultValues from '../../Styles/DefaultValues.module.scss'
-import showcaseStyle from './ShowcaseStyle.module.scss'
-
-const style = { backgroundColor: DefaultValues.secondaryColor, height: showcaseStyle.paperSize, width: showcaseStyle.paperSize, borderRadius: '25px' };
-const style2 = { height: '9rem', width: '9rem', borderRadius: '10px' };
-const style3 = { backgroundColor: DefaultValues.primaryColor, height: '2.5rem', borderRadius: '10px' };
+import { ComposerPaper, GenrePaper } from './StackedPaper'
 
 export default function Showcase() {
-    const handleClick = (e: any) => {
-        e.stopPropagation();
-        console.log('top');
-    }
-
-    const handleHover = (e:any) => {
-        console.log(e)
-    }
 
     return (
         <>
@@ -29,46 +16,34 @@ export default function Showcase() {
 
                 <Grid container>
                     <Grid item container xs={6} lg={3} justifyContent="center">
-                        <Paper elevation={3} sx={style} onClick={() => console.log("tada")} >
-                            <Grid container item minHeight='100%' alignItems="center" justifyContent="center">
-                                {/* <Grid item> */}
-                                <Paper elevation={1} sx={style2} onClick={handleClick} >
-                                    <Grid container direction="column" item minHeight='100%' alignItems="stretch" justifyContent="flex-end">
-                                        <Paper elevation={1} sx={style3} onMouseOver={handleHover} >
-                                            <Typography fontSize={"60%"}>Play Featured Song</Typography>
-                                        </Paper>
-                                    </Grid>
-                                </Paper>
-                                {/* </Grid> */}
-                            </Grid>
-                            {/* </Box> */}
-                        </Paper>
+                        <ComposerPaper />
                     </Grid>
                     <Grid item container xs={6} lg={3} justifyContent="center">
-                        <Paper elevation={3} sx={style} />
+                        <ComposerPaper />
                     </Grid>
                     <Grid item container xs={6} lg={3} justifyContent="center">
-                        <Paper elevation={3} sx={style} />
+                        <ComposerPaper />
                     </Grid>
                     <Grid item container xs={6} lg={3} justifyContent="center">
-                        <Paper elevation={3} sx={style} />
+                        <ComposerPaper />
                     </Grid>
                 </Grid>
 
                 <Typography>Discover Composers By Genre</Typography>
 
+                {/* Use an API that randomly selects genre types */}
                 <Grid container>
                     <Grid item container xs={6} sm={3} justifyContent="center">
-                        <Paper elevation={3} sx={style} />
+                        <GenrePaper genre='Classical'/>
                     </Grid>
                     <Grid item container xs={6} sm={3} justifyContent="center">
-                        <Paper elevation={3} sx={style} />
+                        <GenrePaper genre='Film Score' />
                     </Grid>
                     <Grid item container xs={6} sm={3} justifyContent="center">
-                        <Paper elevation={3} sx={style} />
+                        <GenrePaper genre='Opera' />
                     </Grid>
                     <Grid item container xs={6} sm={3} justifyContent="center">
-                        <Paper elevation={3} sx={style} />
+                        <GenrePaper genre='Symphony' />
                     </Grid>
                 </Grid>
             </Container>
