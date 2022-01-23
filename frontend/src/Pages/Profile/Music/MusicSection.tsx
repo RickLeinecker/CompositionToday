476ecd55f8df0cd8897c63e2a4adfe3725c11a13
Neo <neocamanga@knights.ucf.edu>
2022-01-23 12:26:37 -0500

    fixed constant POST requests and closing modal issue

--- frontend/src/Pages/Profile/Music/MusicSection.tsx
+++ frontend/src/Pages/Profile/Music/MusicSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Alert } from 'react-bootstrap';
 import GenericHandler from '../../../Handlers/GenericHandler';
 import GenericVirtualizedList from '../../../Helper/Generics/GenericVirtualizedList';
@@ -13,67 +13,65 @@ type Props = {
     handleCloseCreate: () => void;
 }
 
-export default function MusicSection({createOpen, handleCloseCreate, userID}: Props) {
+export default function MusicSection({ createOpen, handleCloseCreate, userID }: Props) {
 
     const [response, setResponse] = useState<Array<MusicType> | undefined>(undefined);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
     const [hasChanged, setHasChanged] = useState(false);
 
-    const notifyChange = () => {
-        setHasChanged(value => !value);
-    } 
+    const notifyChange = () => { setHasChanged(value => !value); }
 
     useEffect(() => {
-        async function fetchData(){
+        async function fetchData() {
 
             const handlerObject: GenericHandlerType = {
-                data: JSON.stringify({contentType: "music", userID}),
+                data: JSON.stringify({ contentType: "music", userID }),
                 methodType: "POST",
                 path: "getUserContentByType",
             }
 
-            try{
+            try {
                 let answer = (await GenericHandler(handlerObject));
-                if(answer.error.length > 0){
+                if (answer.error.length > 0) {
                     setError(answer.error);
                     return;
                 }
-                
+
                 setError("");
                 setResponse(await answer.result);
                 setLoading(false);
-                
 
-            } catch(e: any){
+
+            } catch (e: any) {
                 console.error("Frontend Error: " + e);
                 setError(DefaultValues.apiErrorMessage);
             }
-        
+
         }
         fetchData();
-    }, [userID, notifyChange])
+    }, [userID, hasChanged])
 
 
-        
+
     return (
         <>
             <CreateMusicModal userID={userID} notifyChange={notifyChange} createOpen={createOpen} handleCloseCreate={handleCloseCreate} />
             <div>
-                {!error && loading ? <div>...loading</div> 
-                :
-                error ? 
-                <Alert variant="danger">{error}</Alert>
-                : 
-                <div>
-                    <GenericVirtualizedList
-                        bodyStyle={{ width: "100%", height: "50vh" }}
-                        individualStyle={{ padding: "1% 1% 20px" }}
-                        items={response}
-                        notifyChange={notifyChange}
-                        type={"music"}
-                    />
-                </div>
+                {!error && loading ? <div>...loading</div>
+                    :
+                    error ?
+                        <Alert variant="danger">{error}</Alert>
+                        :
+                        <div>
+                            <GenericVirtualizedList
+                                bodyStyle={{ width: "100%", height: "50vh" }}
+                                individualStyle={{ padding: "1% 1% 20px" }}
+                                items={response}
+                                notifyChange={notifyChange}
+                                type={"music"}
+                            />
+                        </div>
                 }
             </div>
         </>
