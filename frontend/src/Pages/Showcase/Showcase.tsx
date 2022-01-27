2491828fcf7511a1df919b08b65220d185c2671c
Neo <neocamanga@knights.ucf.edu>
2022-01-26 19:46:18 -0500

    style additions

--- frontend/src/Pages/Showcase/Showcase.tsx
+++ frontend/src/Pages/Showcase/Showcase.tsx
@@ -4,9 +4,33 @@ import GenericSearch from '../../Helper/Generics/GenericSearch';
 import TopNavBar from '../TopNavBar';
 import ComposerPaper from './ComposerPaper';
 import GenrePaper from './GenrePaper';
+import GenericGetHandler from '../../Handlers/GenericGetHandler';
 import './ShowcaseStyle.scss';
+import { GenericHandlerType } from '../../ObjectInterface';
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
 
 export default function Showcase() {
+
+    const getGenres = async () => {
+        try {
+            let answer = await GenericGetHandler("getComposerGenres");
+            // if (answer.error.length > 0) {
+            //     toast.error('Failed to get data');
+            //     return;
+            // }
+            console.log(answer.result.map((obj:any) => obj.tagName));
+        } catch (e: any) {
+            console.error("Frontend Error: " + e);
+            toast.error('Failed to create experience');
+        }
+    }
+
+    useEffect(() => {
+        getGenres();
+    }, [])
+
+    // GenericGetHandler('getComposerGenres').then(res => { JSON.parse(res).then((ans: any) => { console.log(ans) }) });
 
     return (
         <>
@@ -16,40 +40,44 @@ export default function Showcase() {
                 <GenericSearch />
                 <h1 className="header" >Featured Composers</h1>
 
-                <Grid container>
-                    <Grid item container xs={6} lg={3} justifyContent="center">
-                        <ComposerPaper />
+                <div className="container">
+                    <Grid container>
+                        <Grid item container xs={6} lg={3} justifyContent="center">
+                            <ComposerPaper />
+                        </Grid>
+                        <Grid item container xs={6} lg={3} justifyContent="center">
+                            <ComposerPaper />
+                        </Grid>
+                        <Grid item container xs={6} lg={3} justifyContent="center">
+                            <ComposerPaper />
+                        </Grid>
+                        <Grid item container xs={6} lg={3} justifyContent="center">
+                            <ComposerPaper />
+                        </Grid>
                     </Grid>
-                    <Grid item container xs={6} lg={3} justifyContent="center">
-                        <ComposerPaper />
-                    </Grid>
-                    <Grid item container xs={6} lg={3} justifyContent="center">
-                        <ComposerPaper />
-                    </Grid>
-                    <Grid item container xs={6} lg={3} justifyContent="center">
-                        <ComposerPaper />
-                    </Grid>
-                </Grid>
+                </div>
 
-                
+
 
                 <h1 className="header">Discover Composers By Genre</h1>
 
                 {/* Use an API that randomly selects genre types */}
-                <Grid container>
-                    <Grid item container xs={6} sm={3} justifyContent="center">
-                        <GenrePaper genre='Classical' />
+                <div className="container">
+                    <Grid container>
+                        <Grid item container xs={6} sm={3} justifyContent="center">
+                            <GenrePaper genre='Classical' />
+                        </Grid>
+                        <Grid item container xs={6} sm={3} justifyContent="center">
+                            <GenrePaper genre='Film Score' />
+                        </Grid>
+                        <Grid item container xs={6} sm={3} justifyContent="center">
+                            <GenrePaper genre='Opera' />
+                        </Grid>
+                        <Grid item container xs={6} sm={3} justifyContent="center">
+                            <GenrePaper genre='Symphony' />
+                        </Grid>
                     </Grid>
-                    <Grid item container xs={6} sm={3} justifyContent="center">
-                        <GenrePaper genre='Film Score' />
-                    </Grid>
-                    <Grid item container xs={6} sm={3} justifyContent="center">
-                        <GenrePaper genre='Opera' />
-                    </Grid>
-                    <Grid item container xs={6} sm={3} justifyContent="center">
-                        <GenrePaper genre='Symphony' />
-                    </Grid>
-                </Grid>
+                </div>
             </Container>
         </>
     )
