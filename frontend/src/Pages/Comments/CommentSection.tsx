17dbedda6925afb55b4c5c8c3ba3d67a1b35e64b
Federico Baron <federicoagustinbaron@gmail.com>
2022-02-08 10:56:43 -0500

    add loading and error

--- frontend/src/Pages/Comments/CommentSection.tsx
+++ frontend/src/Pages/Comments/CommentSection.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-bootstrap';
 import GenericHandler from '../../Handlers/GenericHandler';
 import GenericVirtualizedList from '../../Helper/Generics/GenericVirtualizedList';
 import { CommentType, GenericHandlerType } from '../../ObjectInterface';
+import DefaultValues from '../../Styles/DefaultValues.module.scss';
 
 type Props = {
     contentID: number;
@@ -10,6 +12,8 @@ type Props = {
 
 export default function CommentSection({contentID, notifyChange}: Props) {
     const [response, setResponse] = useState<Array<CommentType> | undefined>(undefined);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
 
     useEffect(() => {
         async function fetchData() {
@@ -22,13 +26,18 @@ export default function CommentSection({contentID, notifyChange}: Props) {
             try {
                 let answer = (await GenericHandler(handlerObject));
                 if (answer.error.length > 0) {
+                    setError(answer.error);
                     return;
                 }
 
+                setError("");
                 setResponse(await answer.result);
+                console.log(answer.result);
+                setLoading(false);
 
             } catch (e: any) {
                 console.error("Frontend Error: " + e);
+                setError(DefaultValues.apiErrorMessage);
             }
 
         }
@@ -37,13 +46,21 @@ export default function CommentSection({contentID, notifyChange}: Props) {
     
     return (
         <div>
-            <GenericVirtualizedList
-                bodyStyle={{ width: "100%", height: "20vh" }}
-                individualStyle={{ padding: "1% 1% 20px" }}
-                items={response}
-                notifyChange={notifyChange}
-                type={"comment"}
-            />
+            {!error && loading ? <div>...loading</div>
+                    :
+                    error ?
+                        <Alert variant="danger">{error}</Alert>
+                        :
+                        <div>
+                            <GenericVirtualizedList
+                                bodyStyle={{ width: "100%", height: "20vh" }}
+                                individualStyle={{ padding: "1% 1% 20px" }}
+                                items={response}
+                                notifyChange={notifyChange}
+                                type={"comment"}
+                            />
+                        </div>
+            }
         </div>
     );
 }
