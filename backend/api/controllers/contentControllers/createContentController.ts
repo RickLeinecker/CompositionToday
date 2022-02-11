f5d58b3b8dccdb3640463ca5e81bd6ed8cae5c37
Brian Moon <apollomoon@knights.ucf.edu>
2022-02-11 18:13:14 -0500

    getCommentsForContent, createContent, and getUserContentByType all accept uid instead of userID

--- backend/api/controllers/contentControllers/createContentController.ts
+++ backend/api/controllers/contentControllers/createContentController.ts
@@ -3,12 +3,13 @@ var { mysql_pool } = require("../../../database/database.ts");
 
 // createContent
 exports.createContent = async (req, res) => {
-  // incoming: userID, imageFilepathArray, contentText, location,
+  // incoming: uid, imageFilepathArray, contentText, location,
   // timestamp, audioFilepath, sheetMusicFilepath, contentType,
   // contentName, websiteLink, collaborators, description, mapsEnabled
   // toDate, fromDate, isDateCurrent, price, audioFilename, sheetMusicFilename
   // imageFilepath, imageFilename
   // outgoing: error
+
   // only accept boolean for isDateCurrent
   // note: update endpoints to enforce types for all variables
   var error = "";
@@ -16,7 +17,7 @@ exports.createContent = async (req, res) => {
   var responseCode = 0;
 
   const {
-    userID,
+    uid,
     imageFilepathArray,
     contentName,
     contentText,
@@ -43,51 +44,87 @@ exports.createContent = async (req, res) => {
   // if (isDateCurrent == "true") {
   //   isDateCurrentNum = 1;
   // }
-  mysql_pool.getConnection(function (err, connection) {
-    const sqlInsert =
-      "INSERT INTO content(userID,imageFilepathArray,contentName,contentText,location,timestamp,audioFilepath,sheetMusicFilepath,contentType,websiteLink,collaborators,description,mapsEnabled,toDate,fromDate,isDateCurrent,price,audioFilename,sheetMusicFilename,imageFilepath,imageFilename) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+  mysql_pool.getConnection(async function (err, connection) {
+    // query database, handle errors, return JSON
     connection.query(
-      sqlInsert,
-      [
-        userID,
-        imageFilepathArray,
-        contentName,
-        contentText,
-        location,
-        timestamp,
-        audioFilepath,
-        sheetMusicFilepath,
-        contentType,
-        websiteLink,
-        collaborators,
-        description,
-        mapsEnabled,
-        toDate,
-        fromDate,
-        isDateCurrent,
-        price,
-        audioFilename,
-        sheetMusicFilename,
-        imageFilepath,
-        imageFilename,
-      ],
-      function (err, result) {
+      "SELECT * FROM user WHERE uid=?",
+      [uid],
+      await function (err, result) {
         if (err) {
-          error = err;
+          error = "SQL Search Error";
           responseCode = 500;
           console.log(err);
         } else {
-          results.push("Success");
-          responseCode = 201;
-          // console.log(result);
+          if (result[0]) {
+            responseCode = 200;
+            mysql_pool.getConnection(async function (err, connection) {
+              const sqlInsert =
+                "INSERT INTO content(userID,imageFilepathArray,contentName,contentText,location,timestamp,audioFilepath,sheetMusicFilepath,contentType,websiteLink,collaborators,description,mapsEnabled,toDate,fromDate,isDateCurrent,price,audioFilename,sheetMusicFilename,imageFilepath,imageFilename) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+              connection.query(
+                sqlInsert,
+                [
+                  result[0].id,
+                  imageFilepathArray,
+                  contentName,
+                  contentText,
+                  location,
+                  timestamp,
+                  audioFilepath,
+                  sheetMusicFilepath,
+                  contentType,
+                  websiteLink,
+                  collaborators,
+                  description,
+                  mapsEnabled,
+                  toDate,
+                  fromDate,
+                  isDateCurrent,
+                  price,
+                  audioFilename,
+                  sheetMusicFilename,
+                  imageFilepath,
+                  imageFilename,
+                ],
+                await function (err, result) {
+                  if (err) {
+                    error = err;
+                    responseCode = 500;
+                    console.log(err);
+                  } else {
+                    results.push("Success");
+                    responseCode = 201;
+                    finishedProcess();
+                    // console.log(result);
+                    // console.log("YOOOOO");
+                  }
+                  // package data
+                  // var ret = {
+                  //   result: results,
+                  //   error: error,
+                  // };
+                  // send data
+                  // res.status(responseCode).json(ret);
+                  connection.release();
+                }
+              );
+            });
+          } else {
+            error = "User does not exist";
+            responseCode = 500;
+            finishedProcess();
+          }
+          function finishedProcess() {
+            // package data
+            var ret = {
+              result: results,
+              error: error,
+            };
+            // send data
+            res.status(responseCode).json(ret);
+          }
         }
-        // package data
-        var ret = {
-          result: results,
-          error: error,
-        };
-        // send data
-        res.status(responseCode).json(ret);
+
         connection.release();
       }
     );
