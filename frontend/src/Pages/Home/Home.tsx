e596d892458532df2f48539ffd7b4819d701a67f
Federico Baron <federicoagustinbaron@gmail.com>
2022-02-19 16:35:07 -0500

    homefeed fixes

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -2,31 +2,32 @@ import TopNavBar from '../TopNavBar';
 import GenericInfiniteLoader from '../../Helper/Generics/GenericInfiniteLoader';
 import FilterFeed from './FilterFeed';
 import SortFeed from './SortFeed';
-import { useEffect, useState } from 'react';
-import { GenericHandlerType } from '../../ObjectInterface';
-import GenericHandler from "../../Handlers/GenericHandler";
+import { useState } from 'react';
 import { getAuth } from 'firebase/auth';
 
 export default function Home() {
 
     const [sortBy, setSortBy] = useState<string>("newest");
-    const [filterByType, setFilterByType] = useState<Array<string>>(["music", "event", "article"]);
+    const [filterByType, setFilterByType] = useState<Array<string>>([]);
     const [key, setKey] = useState<number>(0);
     const currentUid = getAuth().currentUser?.uid;
 
-    useEffect(() => {
-        console.log(key)
-        setKey(prev => prev + 1);
-    }, [filterByType, sortBy]);
-
     function updateSortBy(newValue: string) {
-        console.log("sort", newValue)
         setSortBy(newValue);
+        setKey(prev => prev + 1);
     }
 
-    function updateFilterBy(newValue: string[]) {
-        console.log(newValue);
-        setFilterByType(newValue);
+    function updateFilterBy(newValue: string) {
+        let tempArr = filterByType;
+
+        if(tempArr.includes(newValue)){
+            tempArr = tempArr.filter(e => e !== newValue);
+        }
+        else{
+            tempArr.push(newValue)
+        }
+        setFilterByType(tempArr);
+        setKey(prev => prev + 1);
     }
 
     return (
@@ -35,7 +36,7 @@ export default function Home() {
             <div className='container'>
                 <div style={{ position: "relative", display: "flex", justifyContent: "right", marginTop: "1%" }}>
                     <div style={{ marginRight: "1%" }}>
-                        <FilterFeed filterByType={filterByType} updateFilterBy={updateFilterBy} />
+                        <FilterFeed updateFilterBy={updateFilterBy} />
                     </div>
                     <div>
                         <SortFeed sortBy={sortBy || ""} updateSortBy={updateSortBy} />
