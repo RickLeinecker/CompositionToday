a4ee01529ffe9e9b9fd36cc4e9b361e69d8e2f78
Federico Baron <federicoagustinbaron@gmail.com>
2022-02-25 17:14:36 -0500

    add show more

--- frontend/src/Pages/Profile/Articles/ArticleCard.tsx
+++ frontend/src/Pages/Profile/Articles/ArticleCard.tsx
@@ -5,6 +5,7 @@ import EditArticleModal from './EditArticleModal';
 import { Divider } from '@mui/material';
 import CardFooter from '../CardFooter';
 import ArticleCardHeader from './ArticleCardHeader';
+import { useState } from 'react';
 
 type Props = {
     article: ArticleType;
@@ -18,6 +19,8 @@ type Props = {
 export default function ArticleCard({ article, isMyProfile, notifyVirtualizer, notifyChange, clearCache }: Props) {
     const { id, contentName, contentText, username, profilePicPath, displayName, timestamp, likeCount, isLikedByLoggedInUser } = article;
 
+    const [showMore, setShowMore] = useState(false);
+
     return (
         <div className="card">
             <ArticleCardHeader article={article} isMyProfile={false} notifyChange={notifyChange} />
@@ -26,7 +29,11 @@ export default function ArticleCard({ article, isMyProfile, notifyVirtualizer, n
 
             <div className="card-body" style={{ paddingBottom: "0%" }}>
                 <h1 className="card-title">{contentName}</h1>
-                <p className="card-text">{contentText}</p>
+                <p className="card-text">{(showMore || contentText.length <= 250) ? contentText : contentText.substring(0, 250) + "..."}</p>
+                <div style={{float: "right"}}>
+                    {(!showMore && contentText.length > 250) && <p style={{cursor: "pointer", textDecoration: "underline"}} onClick={() => {setShowMore(true); clearCache(); notifyVirtualizer()}}>Show more</p>}
+                    {(showMore && contentText.length > 250) && <p style={{cursor: "pointer", textDecoration: "underline"}} onClick={() => {setShowMore(false); clearCache(); notifyVirtualizer()}}>Show less</p>}
+                </div>
             </div>
 
             <Divider variant="fullWidth" component="div" sx={{ margin: "1% auto", width: "95%" }} />
--- frontend/src/Pages/Profile/Events/EventCard.tsx
+++ frontend/src/Pages/Profile/Events/EventCard.tsx
@@ -19,6 +19,8 @@ export default function EventCard({ event, isMyProfile, notifyVirtualizer, notif
     const { id, contentName, description, fromDate, toDate, imageFilepath, location, mapsEnabled, username, profilePicPath, displayName, timestamp, likeCount, isLikedByLoggedInUser } = event;
     const [showMap, setShowMap] = useState<boolean>(false);
     const src: string = "https://www.google.com/maps/embed/v1/place?key=" + process.env.REACT_APP_GOOGLE_MAPS_API + "&q=" + location
+    const [showMore, setShowMore] = useState(false);
+
 
     const handleMapExpand = () => {
         setShowMap(prev => !prev);
@@ -36,7 +38,12 @@ export default function EventCard({ event, isMyProfile, notifyVirtualizer, notif
                 <div style={{ display: "flex", margin: "0 0" }}>
                     <div style={{ flex: "1 0 0" }}>
                         <h5 className="card-title">{contentName}</h5>
-                        <p className="card-text-secondary">{description}</p>
+                        <p className="card-text-secondary" style={{marginBottom: "0%"}}>{(showMore || !description || description.length <= 250) ? description : description?.substring(0, 250) + "..."}</p>
+                        <div style={{float: "right"}}>
+                            {(!showMore && description && description.length > 250) && <p style={{cursor: "pointer", textDecoration: "underline"}} onClick={() => {setShowMore(true); clearCache(); notifyVirtualizer()}}>Show more</p>}
+                            {(showMore && description && description.length > 250) && <p style={{cursor: "pointer", textDecoration: "underline"}} onClick={() => {setShowMore(false); clearCache(); notifyVirtualizer()}}>Show less</p>}
+                        </div>
+                        <br/>
                         <p className="card-text">{"From: " + moment(new Date(fromDate).toUTCString()).format('MMMM Do YYYY, h:mm:ss a')}</p>
                         <p className="card-text">{"To: " + moment(new Date(toDate).toUTCString()).format('MMMM Do YYYY, h:mm:ss a')}</p>
                         {location && <p className="card-text">{"Location: " + location}</p>}
