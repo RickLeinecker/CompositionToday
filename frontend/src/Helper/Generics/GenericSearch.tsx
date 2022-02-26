3b01670d89c146785409f172480a35ba7e747a58
Neo <neocamanga@knights.ucf.edu>
2022-02-26 00:49:52 -0500

    cleaning code

--- frontend/src/Helper/Generics/GenericSearch.tsx
+++ frontend/src/Helper/Generics/GenericSearch.tsx
@@ -1,69 +1,9 @@
-import { Form, FormControl } from "react-bootstrap";
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import { useCallback, useState } from "react";
 import { GenericHandlerType } from "../../ObjectInterface";
 import GenericHandler from "../../Handlers/GenericHandler";
 import { AsyncTypeahead } from 'react-bootstrap-typeahead';
-
-const SEARCH_URI = 'https://api.github.com/search/users';
-
-const AsyncExample = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [options, setOptions] = useState<object[]>([]);
-
-    const handleSearch = (query: string) => {
-        setIsLoading(true);
-
-        fetch(`${SEARCH_URI}?q=${query}+in:login&page=1&per_page=50`)
-            .then((resp) => resp.json())
-            .then(({ items }) => {
-                const options = items.map((i: any) => ({
-                    avatar_url: i.avatar_url,
-                    id: i.id,
-                    login: i.login,
-                }));
-
-                setOptions(options);
-                setIsLoading(false);
-            });
-    };
-
-    // Bypass client-side filtering by returning `true`. Results are already
-    // filtered by the search endpoint, so no need to do it again.
-    const filterBy = () => true;
-
-    return (
-        <AsyncTypeahead
-            filterBy={filterBy}
-            id="async-example"
-            isLoading={isLoading}
-            labelKey="login"
-            minLength={3}
-            onSearch={handleSearch}
-            options={options}
-            placeholder="Search for a Composition Today User..."
-            renderMenuItemChildren={(option: any, props) => (
-                <>
-                    <img
-                        alt={option.login}
-                        src={option.avatar_url}
-                        style={{
-                            height: '24px',
-                            marginRight: '10px',
-                            width: '24px',
-                        }}
-                    />
-                    <span>{option.login}</span>
-                </>
-            )}
-        />
-    );
-};
-
-const AsyncComposerInput = () => {
-
-}
 
 type Props = {
     placeHolder: string;
@@ -91,7 +31,7 @@ const GenericSearch = ({ placeHolder, apiEndpoint, genre = '', getPayload }: Pro
             methodType: "POST",
             path: apiEndpoint,
         }
-        console.log(handlerObject.data)
+
         try {
             setIsLoading(true);
             let answer = await GenericHandler(handlerObject);
@@ -103,7 +43,6 @@ const GenericSearch = ({ placeHolder, apiEndpoint, genre = '', getPayload }: Pro
             else if (answer.error === "No composers found" || answer.error === "Genre doesn't exist")
                 getPayload([]);
 
-            console.log("tempArr", tempArr)
             setOptions(tempArr);
             setIsLoading(false);
         } catch (e: any) {
@@ -113,38 +52,27 @@ const GenericSearch = ({ placeHolder, apiEndpoint, genre = '', getPayload }: Pro
 
     const handleSubmit = (e: any) => {
         e.preventDefault();
-        console.log("SEARCHING:", search);
         searchComposers(search);
     }
 
     const handleSearch = useCallback((query: string) => {
         query = (query.length < 2) ? "" : query;
         setSearch(query);
-        console.log("QUERYING:", query);
         searchComposers(query);
     }, [])
 
     const handleInputChange = (text: string) => {
         setSearch(text);
     };
+
     // Bypass client-side filtering by returning `true`. Results are already
     // filtered by the search endpoint, so no need to do it again.
     const filterBy = () => true;
 
     return (
-        <div onSubmit={handleSubmit}>
-            {/* <FormControl
-                type="search"
-                placeholder={placeHolder}
-                className="me-2"
-                aria-label="Search"
-                onChange={e => setSearch(e.target.value)}
-                value={search}
-            /> */}
-            {/* <AsyncExample /> */}
+        <>
             <AsyncTypeahead
                 id="Composer Search"
-                // className="me-2"
                 style={{ display: !genre ? "inline-flex" : "inline" }}
                 filterBy={filterBy}
                 isLoading={isLoading}
@@ -154,11 +82,9 @@ const GenericSearch = ({ placeHolder, apiEndpoint, genre = '', getPayload }: Pro
                 open={!genre ? undefined : false}
                 onSearch={handleSearch}
                 options={options}
-                // onChange={(e: any) => setSearch(e.target.value)}
                 onInputChange={handleInputChange}
                 placeholder={placeHolder}
                 renderMenuItemChildren={(option: any, props) => {
-                    console.log(option);
                     return (
                         <div key={option.uid}>
                             <img
@@ -182,7 +108,7 @@ const GenericSearch = ({ placeHolder, apiEndpoint, genre = '', getPayload }: Pro
                     <SearchIcon />
                 </IconButton>
             }
-        </div>
+        </>
     );
 }
 
