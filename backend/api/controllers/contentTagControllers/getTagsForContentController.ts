61472705af59ef97071ce5b164cb93d9d05265fb
Brian Moon <apollomoon@knights.ucf.edu>
2022-02-28 16:17:56 -0500

    createContentWithTags and getTagsForContent

--- backend/api/controllers/contentTagControllers/getTagsForContentController.ts
+++ backend/api/controllers/contentTagControllers/getTagsForContentController.ts
@@ -1,8 +1,6 @@
 // mysql connection
 var { mysql_pool } = require("../../../database/database.ts");
 
-// Note: we need to do the same thing for user tags
-
 // getTagsForContent
 exports.getTagsForContent = async (req, res) => {
   // incoming: contentID
@@ -16,7 +14,8 @@ exports.getTagsForContent = async (req, res) => {
   const { contentID } = req.body;
   mysql_pool.getConnection(function (err, connection) {
     connection.query(
-      "SELECT * FROM contentTag WHERE contentID=?",
+      `SELECT tag.id,contentTag.contentID,tag.tagName 
+      FROM contentTag INNER JOIN tag ON contentTag.tagID=tag.id WHERE contentID=?`,
       [contentID],
       function (err, result) {
         if (err) {
@@ -25,51 +24,21 @@ exports.getTagsForContent = async (req, res) => {
           console.log(err);
         } else {
           if (result[0]) {
-            // after getting contentTags from contentTag table,
-            // get tag information from tag table (tagName)
-            let tagID = result[0].tagID;
-            // run query
-            connection.query(
-              "SELECT * FROM tag WHERE id=?",
-              [tagID],
-              function (err, result) {
-                if (err) {
-                  error = "SQL Search Error";
-                  responseCode = 500;
-                  console.log(err);
-                } else {
-                  if (result[0]) {
-                    results = result[0];
-                    responseCode = 200;
-                  } else {
-                    error = "This tag does not exist";
-                    responseCode = 500;
-                  }
-                }
-                // package data
-                var ret = {
-                  result: results,
-                  error: error,
-                };
-                // send data
-                res.status(responseCode).json(ret);
-                connection.release();
-                return;
-              }
-            );
+            results = result;
+            responseCode = 200;
           } else {
             error = "This content has no tags";
             responseCode = 500;
-            // package data
-            var ret = {
-              result: results,
-              error: error,
-            };
-            // send data
-            res.status(responseCode).json(ret);
-            connection.release();
           }
         }
+        // package data
+        var ret = {
+          result: results,
+          error: error,
+        };
+        // send data
+        res.status(responseCode).json(ret);
+        connection.release();
       }
     );
   });
