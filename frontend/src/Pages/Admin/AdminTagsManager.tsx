1ddf6d4ac79a0aee05fadd5629422ce4c42064b3
Federico Baron <federicoagustinbaron@gmail.com>
2022-02-28 16:30:53 -0500

    add virtualized list

--- frontend/src/Pages/Admin/AdminTagsManager.tsx
+++ frontend/src/Pages/Admin/AdminTagsManager.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
+import GenericGetHandler from '../../Handlers/GenericGetHandler';
+import { TagType } from '../../ObjectInterface';
 import AdminTagsVirtualizedList from './AdminTagsVirtualizedList'
 
 export default function AdminTagsManager() {
 
     const [tagsChanged, setTagsChanged] = useState<boolean>(false);
+    const [responseTags, setResponseTags] = useState<Array<TagType> | undefined>(undefined);
+    const [responseGenres, setResponseGenres] = useState<Array<TagType> | undefined>(undefined);
 
     const notifyChange = () => {
         setTagsChanged(value => !value);
@@ -11,10 +15,44 @@ export default function AdminTagsManager() {
 
     useEffect(() => {
         async function fetchTags() {
+            try {
+                let answer = (await GenericGetHandler("getTags"));
+                if (answer.error.length > 0) {
+                    // setError(answer.error);
+                    return;
+                }
 
+                // setError("");
+                const result = await answer.result;
+                setResponseTags(result);
+
+                // setLoading(false);
+
+
+            } catch (e: any) {
+                console.error("Frontend Error: " + e);
+                // setError(DefaultValues.apiErrorMessage);
+            }
         }
         async function fetchGenres() {
+            try {
+                let answer = (await GenericGetHandler("getComposerGenres"));
+                if (answer.error.length > 0) {
+                    // setError(answer.error);
+                    return;
+                }
 
+                // setError("");
+                const result = await answer.result;
+                setResponseGenres(result);
+
+                // setLoading(false);
+
+
+            } catch (e: any) {
+                console.error("Frontend Error: " + e);
+                // setError(DefaultValues.apiErrorMessage);
+            }
         }
         fetchTags();
         fetchGenres();
@@ -26,26 +64,26 @@ export default function AdminTagsManager() {
                 <p>
                     Tags
                 </p>
-                {/* <AdminTagsVirtualizedList
+                <AdminTagsVirtualizedList
                     bodyStyle={{ width: "100%", height: "30vh" }}
                     individualStyle={{ padding: "1% 1% 20px" }}
                     items={responseTags}
                     notifyChange={notifyChange}
                     type={"tag"}
-                /> */}
+                />
             </div>
 
             <div>
                 <p>
                     Genres
                 </p>
-                {/* <AdminTagsVirtualizedList
+                <AdminTagsVirtualizedList
                     bodyStyle={{ width: "100%", height: "30vh" }}
                     individualStyle={{ padding: "1% 1% 20px" }}
                     items={responseGenres}
                     notifyChange={notifyChange}
                     type={"genre"}
-                /> */}
+                />
             </div>
         </>
     )
