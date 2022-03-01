67e5b75f4be79c8acc8be14ab4d33805fb987fc8
Federico Baron <federicoagustinbaron@gmail.com>
2022-03-01 08:54:20 -0500

    add image upload genre

--- frontend/src/Pages/Admin/CreateGenreModal.tsx
+++ frontend/src/Pages/Admin/CreateGenreModal.tsx
@@ -7,6 +7,9 @@ import GenericInputField from '../../Helper/Generics/GenericInputField';
 import GenericModal from '../../Helper/Generics/GenericModal';
 import { GenericHandlerType } from '../../ObjectInterface';
 import GenericHandler from '../../Handlers/GenericHandler';
+import GenericFileUpload from '../../Helper/Generics/GenericFileUpload';
+import { uploadFile } from '../../Helper/Utils/FileUploadUtil';
+import { Alert } from 'react-bootstrap';
 
 
 type Props = {
@@ -18,8 +21,11 @@ type Props = {
 export default function CreateGenreModal({ notifyChange, createOpen, handleCloseCreate}: Props) {
 
     const [newGenreName, setNewGenreName] = useState("");
+    const [newImage, setNewImage] = useState<File | null>(null);
+    const [newImageFilename, setNewImageFilename] = useState("");
 
     const [tagNameError, setGenreNameError] = useState(false);
+    const [missingImageError, setMissingImageError] = useState(false);
 
     const { open: discardOpen, handleClick: handleOpenDiscard, handleClose: handleCloseDiscard } = useOpen();
 
@@ -35,19 +41,31 @@ export default function CreateGenreModal({ notifyChange, createOpen, handleClose
 
     const clearFields = (): void => {
         setNewGenreName("")
-
         setGenreNameError(false);
+        setNewImage(null);
+        setNewImageFilename("");
+    }
+
+    const updateImage = (newFile: File) => {
+        setNewImage(newFile);
+        setNewImageFilename(newFile.name)
+    }
+
+    const deleteImageFile = () => {
+        setNewImage(null);
+        setNewImageFilename("");
     }
 
     const checkForErrors = (): boolean => {
         let error = false;
-        
+
         error = checkIfEmpty(newGenreName, setGenreNameError) || error;
+        error = checkIfEmpty(newImage, setMissingImageError) || error
 
         return(error)
     }
 
-    function checkIfEmpty(value: string, setError: React.Dispatch<React.SetStateAction<boolean>>): boolean {
+    function checkIfEmpty(value: string | File | null, setError: React.Dispatch<React.SetStateAction<boolean>>): boolean {
         if(!value){
             setError(true);
             return true;
@@ -58,12 +76,24 @@ export default function CreateGenreModal({ notifyChange, createOpen, handleClose
     }
 
     async function confirmCreateHandler() {
+
+        let newContentImagePath = null;
+        if (newImage !== null) {
+            newContentImagePath = await uploadFile(newImage, newImageFilename, "genre image", "uploadImage")
+            if (newContentImagePath === '') {
+                toast.error('Failed to create genre');
+                return;
+            }
+        }
+
         const handlerObject: GenericHandlerType = {
             data: JSON.stringify({
-                tagName: newGenreName,
+                genre: newGenreName,
+                imageFilepath: newContentImagePath,
+                // imageFilename: newImageFilename,
             }),
             methodType: "POST",
-            path: "createGenre",
+            path: "addGenre",
         }
 
         try {
@@ -97,6 +127,8 @@ export default function CreateGenreModal({ notifyChange, createOpen, handleClose
             >
                 <div>
                     <GenericInputField title="Genre Name" type="tagName" onChange={setNewGenreName} value={newGenreName} isRequired={true} error={tagNameError} maxLength={parseInt(DefaultValues.maxLengthShort)}/>
+                    <GenericFileUpload updateFile={updateImage} deleteFile={deleteImageFile} type={"image/*"} name="image" filename={newImageFilename} />
+                    {missingImageError && <Alert variant="danger">{"You must upload an image"}</Alert>}
                 </div>
             </GenericModal>
             <GenericDiscardModal notifyChange={notifyChange} discardOpen={discardOpen} handleCloseDiscard={handleCloseDiscard} handleConfirmDiscard={handleConfirmDiscard}/>
