99ef58b7fd9a85e7cbeb16e6a3289daa474ab741
Neo <neocamanga@knights.ucf.edu>
2022-04-04 19:32:07 -0400

    changed marquee message

--- frontend/src/Pages/Showcase/ComposerPaper.tsx
+++ frontend/src/Pages/Showcase/ComposerPaper.tsx
@@ -13,7 +13,7 @@ export default function ComposerPaper({ composer }: ComposerProps) {
         <Paper elevation={3} sx={style} onClick={() => console.log("tada")} >
             <Grid container item minHeight='100%' alignItems="center" justifyContent="center" >
                 <Marquee style={{ ...marqueeStyle, top: '10px' }} delay={2} pauseOnHover gradientWidth={"20%"} gradientColor={[69, 171, 255]}>
-                    {composer.audioFilename !== null ? composer.audioFilename : "Composer has no featured piece"}
+                    {composer.audioFilename !== null ? composer.audioFilename : "This composer has no featured piece🚫🎵"}
                 </Marquee>
                 <ComposerSubPaper audioFilename={composer.audioFilename} audioFilepath={composer.audioFilepath} imagePath={composer.profilePicPath} username={composer.username} />
                 {/* TODO: Change from username to displayname */}
