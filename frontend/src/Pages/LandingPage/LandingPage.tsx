00fee30a1123433b91c40a9c4a8e54fb70b1147c
Federico Baron <federicoagustinbaron@gmail.com>
2022-05-11 11:22:42 -0400

    fix issue with style

--- frontend/src/Pages/LandingPage/LandingPage.tsx
+++ frontend/src/Pages/LandingPage/LandingPage.tsx
@@ -10,15 +10,15 @@ export default function LandingPage() {
         <>
             <div style={{ height: "100vh" }} className="bg">
                 <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                    <h1 style={{ margin: "2%" }}>Composition Today</h1>
+                    <h1 className="landing-text" style={{ margin: "2%" }}>Composition Today</h1>
                     <div style={{ display: "flex", flex: "1 0 0", justifyContent: "flex-end", alignItems: "center" }}>
                         <h1 className="button" onClick={() => navigate('/registration')}>Register</h1>
-                        <h1 style={{ margin: "2%", cursor: "pointer" }} onClick={() => navigate('/registration')}>Log in</h1>
+                        <h1 className="landing-text" style={{ margin: "2%", cursor: "pointer" }} onClick={() => navigate('/registration')}>Log in</h1>
                     </div>
                 </div>
                 <div style={{ position: "absolute", marginLeft: "2%", marginTop: "26%", width: "25%" }}>
-                    <h1 style={{ marginBottom: "6%" }}>Welcome to Composition Today!</h1>
-                    <h2>A hub for musicians to connect with publishers and with each other</h2>
+                    <h1 className="landing-text" style={{ marginBottom: "6%" }}>Welcome to Composition Today!</h1>
+                    <h2 className="landing-text">A hub for musicians to connect with publishers and with each other</h2>
                 </div>
 
             </div>
