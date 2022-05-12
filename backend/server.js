94982081fbf479bd685bd5b7708475b1ea069704
Brian Moon <apollomoon@knights.ucf.edu>
2022-05-12 14:19:01 -0400

    cors updates

--- backend/server.js
+++ backend/server.js
@@ -66,6 +66,7 @@ app.use("/", relatedProjectsRoutes);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
--- frontend/src/Pages/Profile/Music/MusicCard.tsx
+++ frontend/src/Pages/Profile/Music/MusicCard.tsx
@@ -1,117 +1,184 @@
-import { MusicType } from '../../../ObjectInterface';
-import ReactAudioPlayer from 'react-audio-player';
-import { Divider } from '@mui/material';
-import CardFooter from '../CardFooter';
-import MusicCardHeader from './MusicCardHeader';
-import { useState } from 'react';
-import { Image } from 'react-bootstrap'
-import Waveform from './Player/Waveform';
-
+import { MusicType } from "../../../ObjectInterface";
+import ReactAudioPlayer from "react-audio-player";
+import { Divider } from "@mui/material";
+import CardFooter from "../CardFooter";
+import MusicCardHeader from "./MusicCardHeader";
+import { useState } from "react";
+import { Image } from "react-bootstrap";
+import Waveform from "./Player/Waveform";
 
 type Props = {
-    music: MusicType;
-    isMyProfile: boolean;
-    notifyVirtualizer: () => void;
-    notifyChange: () => void;
-    clearCache: () => void;
-}
+  music: MusicType;
+  isMyProfile: boolean;
+  notifyVirtualizer: () => void;
+  notifyChange: () => void;
+  clearCache: () => void;
+};
 
+export default function MusicCard({
+  music,
+  isMyProfile,
+  notifyVirtualizer,
+  notifyChange,
+  clearCache,
+}: Props) {
+  const {
+    id,
+    contentName,
+    description,
+    audioFilepath,
+    sheetMusicFilepath,
+    timestamp,
+    contentText,
+    username,
+    profilePicPath,
+    displayName,
+    likeCount,
+    isLikedByLoggedInUser,
+    tagArray,
+    imageFilepath,
+    imageFilename,
+  } = music;
 
-export default function MusicCard({ music, isMyProfile, notifyVirtualizer, notifyChange, clearCache }: Props) {
-    const {
-        id,
-        contentName,
-        description,
-        audioFilepath,
-        sheetMusicFilepath,
-        timestamp,
-        contentText,
-        username,
-        profilePicPath,
-        displayName,
-        likeCount,
-        isLikedByLoggedInUser,
-        tagArray,
-        imageFilepath,
-        imageFilename,
-    } = music;
+  const [showMore, setShowMore] = useState(false);
 
-    const [showMore, setShowMore] = useState(false);
+  // Cleanup function gets called when component is unmounted
+  // off the virtualized window.
+  // useEffect(() => {
+  //     console.log("remounted")
+  //     return () => {
+  //         // clearCache();
+  //         setIsCommentsOpen(false);
+  //         console.log("unmounted");
+  //         vRef?.current?.recomputeRowHeights(1);
+  //         vRef?.current?.forceUpdateGrid();
+  //     };
+  // }, [])
 
-    // Cleanup function gets called when component is unmounted
-    // off the virtualized window.
-    // useEffect(() => {
-    //     console.log("remounted")
-    //     return () => {
-    //         // clearCache();
-    //         setIsCommentsOpen(false);
-    //         console.log("unmounted");
-    //         vRef?.current?.recomputeRowHeights(1);
-    //         vRef?.current?.forceUpdateGrid();
-    //     };
-    // }, [])
+  return (
+    <div className="card">
+      <MusicCardHeader
+        music={music}
+        isMyProfile={false}
+        notifyChange={notifyChange}
+      />
 
-    return (
-        <div className="card">
-            <MusicCardHeader music={music} isMyProfile={false} notifyChange={notifyChange} />
+      <Divider
+        variant="fullWidth"
+        component="div"
+        sx={{ margin: "0.5% auto", width: "95%" }}
+      />
 
-            <Divider variant="fullWidth" component="div" sx={{ margin: "0.5% auto", width: "95%" }} />
+      <div
+        className="card-body"
+        style={{ paddingBottom: "0%", display: "flex" }}
+      >
+        <div>
+          {imageFilepath ? (
+            <div>
+              <Image
+                src={imageFilepath}
+                style={{
+                  height: "auto",
+                  width: "auto",
+                  maxHeight: "30vh",
+                  maxWidth: "100%",
+                  overflow: "hidden",
+                  float: "left",
+                }}
+              />
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
 
-            <div className="card-body" style={{ paddingBottom: "0%", display: "flex" }}>
-                <div>
-                    {imageFilepath ?
-                        <div>
-                            <Image src={imageFilepath} style={{ height: "auto", width: "auto", maxHeight: "30vh", maxWidth: "100%", overflow: "hidden", float: "left" }} />
-                        </div>
-                        :
-                        <></>
-                    }
-                </div>
-
-                <div style={{ marginLeft: "5%", display: "flex", flexDirection: "column", flexGrow: "1" }}>
-                    <h5 className="card-title" style={{ margin: "0%" }}>{contentName}</h5>
-                    <p className="card-text" style={{ margin: "0%" }}>{contentText}</p>
-                    <p className="card-text-secondary" style={{ margin: "0%" }}>{(showMore || !description || description.length <= 250) ? description : description?.substring(0, 250) + "..."}</p>
-                    <div style={{ float: "right" }}>
-                        {(!showMore && description && description.length > 250) && <p style={{ cursor: "pointer", textDecoration: "underline" }} onClick={() => { setShowMore(true); clearCache(); notifyVirtualizer() }}>Show more</p>}
-                        {(showMore && description && description.length > 250) && <p style={{ cursor: "pointer", textDecoration: "underline" }} onClick={() => { setShowMore(false); clearCache(); notifyVirtualizer() }}>Show less</p>}
-                    </div>
-                    {sheetMusicFilepath &&
-                        <a href={sheetMusicFilepath} style={{textDecoration:"underline", color: "blue"}} target="_blank" rel="noreferrer">
-                            Open sheet music
-                        </a>
-                    }
-                    <div>
-                        {audioFilepath &&
-                            <>
-                                {/* <ReactAudioPlayer
+        <div
+          style={{
+            marginLeft: "5%",
+            display: "flex",
+            flexDirection: "column",
+            flexGrow: "1",
+          }}
+        >
+          <h5 className="card-title" style={{ margin: "0%" }}>
+            {contentName}
+          </h5>
+          <p className="card-text" style={{ margin: "0%" }}>
+            {contentText}
+          </p>
+          <p className="card-text-secondary" style={{ margin: "0%" }}>
+            {showMore || !description || description.length <= 250
+              ? description
+              : description?.substring(0, 250) + "..."}
+          </p>
+          <div style={{ float: "right" }}>
+            {!showMore && description && description.length > 250 && (
+              <p
+                style={{ cursor: "pointer", textDecoration: "underline" }}
+                onClick={() => {
+                  setShowMore(true);
+                  clearCache();
+                  notifyVirtualizer();
+                }}
+              >
+                Show more
+              </p>
+            )}
+            {showMore && description && description.length > 250 && (
+              <p
+                style={{ cursor: "pointer", textDecoration: "underline" }}
+                onClick={() => {
+                  setShowMore(false);
+                  clearCache();
+                  notifyVirtualizer();
+                }}
+              >
+                Show less
+              </p>
+            )}
+          </div>
+          {sheetMusicFilepath && (
+            <a
+              href={sheetMusicFilepath}
+              style={{ textDecoration: "underline", color: "blue" }}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open sheet music
+            </a>
+          )}
+          <div>
+            {audioFilepath && (
+              <>
+                {/* <ReactAudioPlayer
                                     src={audioFilepath}
                                     autoPlay={false}
                                     controls
                                 /> */}
-                                <Waveform url={audioFilepath} />
-                            </>
-
-                        }
-                    </div>
-                </div>
-
-
-            </div>
-
-            <Divider variant="fullWidth" component="div" sx={{ margin: "1% auto", width: "95%" }} />
-
-            <div>
-                <CardFooter
-                    clearCache={clearCache}
-                    notifyVirtualizer={notifyVirtualizer}
-                    notifyChange={notifyChange}
-                    id={id}
-                    likeCount={likeCount}
-                    isLikedByLoggedInUser={isLikedByLoggedInUser}
-                />
-            </div>
-
+                <Waveform url={audioFilepath} />
+              </>
+            )}
+          </div>
         </div>
-    )
+      </div>
+
+      <Divider
+        variant="fullWidth"
+        component="div"
+        sx={{ margin: "1% auto", width: "95%" }}
+      />
+
+      <div>
+        <CardFooter
+          clearCache={clearCache}
+          notifyVirtualizer={notifyVirtualizer}
+          notifyChange={notifyChange}
+          id={id}
+          likeCount={likeCount}
+          isLikedByLoggedInUser={isLikedByLoggedInUser}
+        />
+      </div>
+    </div>
+  );
 }
